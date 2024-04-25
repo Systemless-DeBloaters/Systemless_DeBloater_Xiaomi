dde25664d3926f1bcc2eff0ca19fd5c6395133d6
Nima <smartnimaco@gmail.com>
2024-04-26 00:22:34 +0330

    Added latest updates, getting ready to release

--- Tools/bpi.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-DATE=$(date '+%Y-%m-%d')
-
-bash Tools/buildpush.sh
-adb root && adb shell -- magisk --install-module /sdcard/Systemless-DeBloater-Xiaomi-$DATE.zip
--- Tools/buildpush.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-DATE=$(date '+%Y-%m-%d')
-
-bash Tools/build.sh
-
-adb shell -- rm -f /sdcard/Systemless-DeBloater-Xiaomi-$DATE.zip && adb push Systemless-DeBloater-Xiaomi-$DATE.zip /sdcard && rm Systemless-DeBloater-Xiaomi-$DATE.zip
